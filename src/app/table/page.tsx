f5dafb6071bc1b052dd79a09b944dc890709f6c8
waqas khalid <waqas.khalid@basecampdata.com>
2023-10-18 15:08:46 +0500

    setup shadcn ui and next ui

---
+++ src/app/table/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div className="m-7">
+      <Button> lets build table</Button>
+    </div>
+  );
+}
